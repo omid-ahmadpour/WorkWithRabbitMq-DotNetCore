df772e3150ac72b348b5e1a8f7b51db07fb0bc5e
OMID <ahmadpooromid@gmail.com>
2021-04-23 20:30:16 +0430

    Add project files.

---
+++ RabbitMq.Consumer/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+
+namespace RabbitMq.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare("RabbitMqTestQueue", true, false, false);
+
+            //for fanout exchange type
+            channel.QueueBind("RabbitMqTestQueue", "TestAppExchange", "");
+
+            //for direct exchange type
+            //channel.QueueBind("RabbitMqTestQueue", "TestAppExchange", "direct.routingKey");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, eventArgs) =>
+            {
+                var msg = System.Text.Encoding.UTF8.GetString(eventArgs.Body.ToArray());
+                Console.WriteLine(msg);
+            };
+
+            channel.BasicConsume("RabbitMqTestQueue", true, consumer);
+
+            Console.ReadLine();
+
+            channel.Close();
+            connection.Close();
+        }
+    }
+}
---
+++ RabbitMq.Producer/Controllers/ClientController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using System;
+
+namespace RabbitMq.Producer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        [HttpGet("Fanout")]
+        public void Fanout()
+        {
+            var message = "omid; 28, ahmadpooromid@gmail.com, Fanout Exchange";
+
+            SendMessage("", message);
+        }
+
+        [HttpGet("Direct")]
+        public void Direct()
+        {
+            var message = "This is a message for Direct Exchange.";
+
+            SendMessage("direct.routingKey", message);
+        }
+
+        private void SendMessage(string routingKey, string message)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare("TestAppExchange", ExchangeType.Direct, true);
+
+            var bytes = System.Text.Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish("TestAppExchange", routingKey, null, bytes);
+
+            channel.Close();
+            connection.Close();
+        }
+    }
+}
